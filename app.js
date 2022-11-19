4f8e8a69694253f436f63352d17a194cc7afeaba
extizout <p.preedee.poom@gmail.com>
2022-11-20 05:36:39 +0700

    Change Variable for passing Content via EJS

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/" , (req,res)=>{
-  res.render("home",{homeStartingContent: homeStartingContent});
+  res.render("home",{Content: homeStartingContent});
 
 });
 
