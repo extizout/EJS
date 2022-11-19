d08e0d067d38681333b4de9e76aaa8693087f31e
extizout <p.preedee.poom@gmail.com>
2022-11-20 05:31:57 +0700

    Create Partials Directory For Header & Footer

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/" , (req,res)=>{
-  res.render("home");
+  res.render("home",{homeStartingContent: homeStartingContent});
+
 });
 
 
